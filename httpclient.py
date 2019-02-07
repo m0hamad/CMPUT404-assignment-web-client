73503cc5e7e691776f33f54f1b01d18331c15c6a
m0hamad <jamaledd@ualberta.ca>
2019-02-06 23:02:23 -0700

    Done and Cleaned Up

--- httpclient.py
+++ httpclient.py
@@ -18,10 +18,10 @@
 # Write your own HTTP GET and POST
 # The point is to understand what you have to send and get experience with it
 
-import sys
-import socket
 import random
 import re
+import sys
+import socket
 # you may use urllib to encode data appropriately
 import urllib.parse
 
@@ -30,148 +30,203 @@ def help():
 
 class HTTPResponse(object):
 
-    def __init__(self, code=200, body=""):
+    def __init__(self, code = 200, body = ""):
+
         self.code = code
         self.body = body
+        
+    def __str__(self):
+
+        return str(self.code) + ", " + self.body
 
 class HTTPClient(object):
 
-    #def get_host_port(self,url):
-
-    def parse_url(self, url):
-
-        parsed = urllib.parse.urlparse(url)
-        host = parsed.hostname
-        path = parsed.path
-        port = parsed.port
-        scheme = parsed.scheme
-
-        return host, path, port, scheme
-
-    def get_port(self, port, scheme):
-
-        if scheme == "http":
-            return 80
-        elif scheme == "https":
-            return 443
-        else:
-            return (27600 + random.randint(1,100))
-
-    def connect(self, host, port):
-
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-        print(host)
-        print(port)
-        
-        
-        self.socket.connect((host, port))
-
-    def get_code(self, data):
-
-        try:
-            code = int(data.split("")[1])
-        except:
-            code = 400
-
-        return code
-
-    def get_headers(self,data):
-
-        try:
-            headers = data.split("\r\n\r\n")[0]
-
-        except:
-            headers = ""
-
-        return headers
-
-    def get_body(self, data):
-
-        body = data.find("\r\n\r\n")
-        
-        if body != -1:
-            return data[body + 4:]
-        else:
-            return ('')
-    
-    def sendall(self, data):
-
-        self.socket.sendall(data.encode('utf-8'))
-        
+    # Mark the socket closed.
     def close(self):
 
         self.socket.close()
 
-    # read everything from the socket
+    # Connect to a remote socket at address.
+    def connect(self, host, port):
+
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect((host, port))
+
+    def get_body(self, data):
+
+        try:
+
+            body = data.split("\r\n\r\n")[1]
+
+        except:
+
+            body = ''
+
+        return body
+
+    # Get the status code: 2xx, 3xx, 4xx, 5xx
+    def get_code(self, data):
+
+        try:
+
+            code = int(data.split(" ")[1])
+
+        except:
+
+            code = 400
+
+        return code
+
+    def get_port(self, port, scheme):
+
+        if scheme == "http":
+
+            return 80
+
+        elif scheme == "https":
+
+            return 443
+
+        else:
+            return 27600 + random.randint(1,100)
+
+    def get_request(self, hostname, path, query, method):
+
+        if method == "POST ":
+
+            method += path + " HTTP/1.1\r\n"
+            host = "Host: " + hostname + "\r\n"
+            content_length = "Content-Length: " + str(len(query)) + "\r\n"
+            content_type = "Content-Type: application/x-www-form-urlencoded\r\n"
+        
+            if len(query) > 0:
+
+                connection = "Connection: close\r\n\r\n" + query + "\r\n"
+
+            else:
+            
+                connection = "Connection: close\r\n\r\n"
+
+            return method + host + content_length + content_type + connection
+
+        else:
+
+            method += path + " HTTP/1.1\r\n"
+            host = "Host: " + hostname + "\r\n"
+            connection = "Connection: close\r\n\r\n"
+
+            return method + host + connection
+    
+    # Tranform dictionary into key and value lists.
+    def get_query(self, args):
+
+        keys = list(args.keys())
+        values = list(args.values())
+        
+        query = ''
+        
+        for i in range(len(keys)):
+
+            query += keys[i] + '=' + values[i] + '&'
+            
+        query = query[:-1]
+
+        return query
+
+    # https://docs.python.org/3/library/urllib.parse.html#url-parsing
+    def parse_url(self, url):
+
+        parsedURL = urllib.parse.urlparse(url)
+        scheme = parsedURL.scheme
+        hostname = parsedURL.hostname
+        port = parsedURL.port
+        path = parsedURL.path
+        
+        if path == "":
+            path = "/"
+
+        if port == None:
+
+            port = self.get_port(port, scheme)
+
+        return scheme, hostname, port, path
+
+    # Read everything from the socket.
     def recvall(self, sock):
 
         buffer = bytearray()
         done = False
 
         while not done:
+
             part = sock.recv(1024)
+
             if (part):
+
                 buffer.extend(part)
+
             else:
+
                 done = not part
+
         return buffer.decode('utf-8')
+    
+    # Send data to the socket.
+    def sendall(self, data):
 
-    def GET(self, url, args=None):
+        self.socket.sendall(data.encode('utf-8'))
 
-        GET_scheme, GET_host, GET_port, GET_path = self.parse_url(url)
-        
-        
-        if GET_port == None:
-            GET_port = self.get_port(GET_port, GET_scheme)
-            
-        self.connect(GET_host, GET_port)
-        
-        # To handle http://Slashdot.org
-        if GET_path == "":
-            GET_request = "GET / HTTP/1.1\nHost: " + GET_host + "\n\n"
-        else:
-            GET_request = "GET " + GET_path + " HTTP/1.1\nHost: " + GET_host + "\n\n"
-        
-        self.sendall(GET_request)
+    def GET(self, url, args = None):
+
+        scheme, hostname, port, path = self.parse_url(url)
+
+        self.connect(hostname, port)
+
+        request = self.get_request(hostname, path, "", "GET ")
+
+        self.sendall(request)
+
         data = self.recvall(self.socket)
-        code = self.get_code(data)
-        body = self.get_body(data)
-        #print(body)
+        status_code = self.get_code(data)
+        content = self.get_body(data)
+
         self.close()
+        
+        return HTTPResponse(status_code, content)
 
-        return HTTPResponse(code, body)
+    def POST(self, url, args = None):
 
-    def POST(self, url, args=None):
+        scheme, hostname, port, path = self.parse_url(url)
 
-        POST_scheme, POST_host, POST_port, POST_path = self.parse_url(url)
-        self.connect(POST_host, POST_port)
+        self.connect(hostname, port)
         
         if (args != None):
-            dictlist = []
-            args = list(args.items())
-            for key, value in args:
-                temp = key + "=" + value
-                dictlist.append(temp)
-            args = "&".join(dictlist)
-            POST_request = "POST " + POST_path + " HTTP/1.1\nHost: " + POST_host + "\nContent-Length: " + str(len(args)) + "\nContent-Type: application/x-www-form-urlencoded" + "\n\n" + args + "\n"         
+
+            query = self.get_query(args)
+            request = self.get_request(hostname, path, query, "POST ")
+
         else:
-            POST_request = "POST " + POST_path + " HTTP/1.1\nHost: " + POST_host + "\nContent-Length: 0" + "\nContent-Type: application/x-www-form-urlencoded" + "\n\n"
             
-        self.sendall(POST_request)
+            request = self.get_request(hostname, path, "", "POST ")
+    
+        self.sendall(request)
+
         data = self.recvall(self.socket)
-        code = self.get_code(data)
-        body = self.get_body(data)
+        status_code = self.get_code(data)
+        content = self.get_body(data)
 
         self.close()        
-        
-        return HTTPResponse(code, body)
 
-    def command(self, url, command="GET", args=None):
+        return HTTPResponse(status_code, content)
+
+    def command(self, url, command = "GET", args = None):
 
         if (command == "POST"):
+
             return self.POST(url, args)
+
         else:
+
             return self.GET(url, args)
     
 if __name__ == "__main__":
